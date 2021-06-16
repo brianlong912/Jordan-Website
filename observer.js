7d27f0ab7530096cdec537c985e827e986723609
Brian Long <brianlong912@gmail.com>
2021-06-16 16:51:10 -0400

    first commit

---
+++ observer.js
@@ -0,0 +1,16 @@
+
+const faders = document.querySelectorAll('.fade-in')
+console.log(faders);
+const opt = {rootMargin: "-200px"};
+
+const faderObserver = new IntersectionObserver(function (entries, faderObserver) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return
+    entry.target.classList.add("appear")
+    faderObserver.unobserve(entry.target)
+  })
+}, opt)
+
+faders.forEach(fader => {
+  faderObserver.observe(fader)
+})
